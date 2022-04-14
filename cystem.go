bc6a86320755d39fcf61e853a65d150c17fe08b3
aiocat <aiocat@protonmail.com>
2022-04-14 14:12:31 +0300

    first commit

---
+++ cystem.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 aiocat
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package cystem
+
+func Run(command string) int {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/aiocat/cystem
+
+go 1.18
