6e0acc42d53d88d789c78664e801992a8dc3fc59
aiocat <aiocat@protonmail.com>
2022-04-14 14:14:06 +0300

    add cystem c header

---
+++ cystem.c
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2022 aiocat
+ * 
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+int cystem(const char* command) {
+    return system(command);
+}
---
+++ cystem.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 aiocat
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#include <stdlib.h>
+
+int cystem(const char* command);
